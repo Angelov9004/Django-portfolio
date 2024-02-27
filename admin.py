c2d4c30418c50a7af66889fced264fd60510ec00
Angelov9004 <136641015+Angelov9004@users.noreply.github.com>
2024-02-27 18:22:02 +0200

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import expirience,university,education,resume,counts,skills,interests,about,Home,contact,services,portfolio
+
+admin.site.register(Home)
+admin.site.register(about)
+admin.site.register(counts)
+admin.site.register(skills)
+admin.site.register(interests)
+admin.site.register(resume)
+admin.site.register(education)
+admin.site.register(university)
+admin.site.register(expirience)
+admin.site.register(contact)
+admin.site.register(services)
+admin.site.register(portfolio)
+
